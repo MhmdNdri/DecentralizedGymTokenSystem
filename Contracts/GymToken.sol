6856d9c8b0669e24aca41de6e5cd31394ce5530d
MhmdNdri <m.n.mohammad.naderi@gmail.com>
2024-12-05 19:36:50 +0000

    add: gymChallanges added to the gymToken contract

--- Contracts/GymToken.sol
+++ Contracts/GymToken.sol
@@ -23,9 +23,21 @@ contract GymToken is ERC20, Pausable, AccessControl {
         Annual
     }
 
-    mapping(MEMBERSHIP_TYPE => uint256) public membershipPrices; // Membership prices by type
-    mapping(address => uint256) public membershipExpiry; // Track expiry timestamps for members
+    struct Challenge {
+        uint256 id;
+        string name;
+        uint256 reward;
+    }
+
+    // Mapping of user registrations for challenges
+    mapping(address => uint256) public userChallengeRegistrations;
+    mapping(MEMBERSHIP_TYPE => uint256) public membershipPrices;
+    mapping(address => uint256) public membershipExpiry;
     mapping(address => uint256) public referralBonuses;
+
+    // Challenges mapping by ID
+    mapping(uint256 => Challenge) public challenges;
+    uint256 public challengeCount;
 
     event MembershipPurchased(
         address indexed member,
@@ -34,6 +46,20 @@ contract GymToken is ERC20, Pausable, AccessControl {
     );
     event ReferralRewarded(address indexed referrer, uint256 amount);
     event StaffPaid(address indexed staff, uint256 amount);
+    event ChallengeCreated(
+        uint256 indexed challengeId,
+        string challengeName,
+        uint256 reward
+    );
+    event ChallengeRegistered(
+        address indexed member,
+        uint256 indexed challengeId
+    );
+    event ChallengeCompleted(
+        address indexed member,
+        uint256 indexed challengeId,
+        uint256 reward
+    );
 
     constructor(address manager) ERC20("GymToken", "GYM") {
         // Assign the manager both admin and manager roles
@@ -61,12 +87,6 @@ contract GymToken is ERC20, Pausable, AccessControl {
         _;
     }
 
-    // Modifier to validate non-zero address
-    modifier validAddress(address account) {
-        require(account != address(0), "Invalid address");
-        _;
-    }
-
     // Allows managers to pause all token-related activities
     function pause() public onlyRole(MANAGER_ROLE) {
         _pause();
@@ -81,14 +101,14 @@ contract GymToken is ERC20, Pausable, AccessControl {
     function addUserToRole(
         address account,
         bytes32 role
-    ) external onlyRole(MANAGER_ROLE) validAddress(account) {
+    ) external onlyRole(MANAGER_ROLE) {
         grantRole(role, account);
     }
 
     function removeUserFromRole(
         address account,
         bytes32 role
-    ) external onlyRole(MANAGER_ROLE) validAddress(account) {
+    ) external onlyRole(MANAGER_ROLE) {
         revokeRole(role, account);
     }
 
@@ -98,6 +118,7 @@ contract GymToken is ERC20, Pausable, AccessControl {
         uint256 amount
     ) public whenNotPaused onlyRole(MANAGER_ROLE) {
         require(balanceOf(msg.sender) >= amount, "Insufficient balance");
+        require(hasRole(STAFF_ROLE, staff), "This is not a staff address");
         _transfer(msg.sender, staff, amount);
     }
 
@@ -105,16 +126,12 @@ contract GymToken is ERC20, Pausable, AccessControl {
     function rewardReferral(
         address referrer
     ) public whenNotPaused onlyRole(MANAGER_ROLE) {
-        // Ensure that the referrer has an active membership
         require(
             block.timestamp < membershipExpiry[referrer],
             "Referrer membership expired"
         );
-
-        // Reward the referrer with the specified amount
         referralBonuses[referrer] += REFERRAL_REWARD;
         _mint(referrer, REFERRAL_REWARD);
-
         emit ReferralRewarded(referrer, REFERRAL_REWARD);
     }
 
@@ -129,7 +146,6 @@ contract GymToken is ERC20, Pausable, AccessControl {
 
         uint256 duration;
 
-        // Set the duration based on membership type
         if (membershipType == MEMBERSHIP_TYPE.Monthly) {
             duration = 30 days;
         } else if (membershipType == MEMBERSHIP_TYPE.Quarterly) {
@@ -140,7 +156,6 @@ contract GymToken is ERC20, Pausable, AccessControl {
             revert("Invalid membership type");
         }
 
-        // If the member has an existing membership, extend it; otherwise, set a new expiration time
         if (block.timestamp < membershipExpiry[msg.sender]) {
             membershipExpiry[msg.sender] += duration;
         } else {
@@ -160,9 +175,71 @@ contract GymToken is ERC20, Pausable, AccessControl {
             return membershipExpiry[member] - block.timestamp;
         }
     }
-}
 
-// examples:
-// staff address: 0xAb8483F64d9C6d1EcF9b849Ae677dD3315835cb2
-// example member address: 0x4B20993Bc481177ec7E8f571ceCaE8A9e22C02db
-// personal trainer address: 0x78731D3Ca6b7E34aC0F824c42a7cC18A495cabaB
+    // Public mint function restricted to managers
+    function mintTokens(
+        address to,
+        uint256 amount
+    ) external onlyRole(MANAGER_ROLE) {
+        require(to != address(0), "Invalid address");
+        _mint(to, amount);
+    }
+
+    // Challenge functions
+
+    // Create a new challenge
+    function createChallenge(
+        string memory name,
+        uint256 reward
+    ) external onlyManager {
+        challengeCount++;
+        challenges[challengeCount] = Challenge({
+            id: challengeCount,
+            name: name,
+            reward: reward
+        });
+
+        emit ChallengeCreated(challengeCount, name, reward);
+    }
+
+    // Register for a challenge
+    function registerForChallenge(uint256 challengeId) external onlyMember {
+        // Ensure the challenge exists
+        Challenge storage challenge = challenges[challengeId];
+        require(challenge.id != 0, "Challenge does not exist");
+
+        // Register the user for the challenge
+        userChallengeRegistrations[msg.sender] = challengeId;
+
+        emit ChallengeRegistered(msg.sender, challengeId);
+    }
+
+    // Complete a challenge and reward the member
+    function completeChallenge() external onlyMember {
+        uint256 challengeId = userChallengeRegistrations[msg.sender];
+        require(challengeId != 0, "No active challenge registered");
+
+        Challenge storage challenge = challenges[challengeId];
+
+        // Reward the member
+        _mint(msg.sender, challenge.reward);
+
+        // Clear the member's registration for the challenge
+        userChallengeRegistrations[msg.sender] = 0;
+
+        emit ChallengeCompleted(msg.sender, challengeId, challenge.reward);
+    }
+
+    // View a challenge's details by ID
+    function getChallengeDetails(
+        uint256 challengeId
+    ) external view returns (string memory, uint256) {
+        Challenge storage challenge = challenges[challengeId];
+        return (challenge.name, challenge.reward);
+    }
+}
+// addresses:
+// Manager: 0x5B38Da6a701c568545dCfcB03FcB875f56beddC4
+// staff: 0xAb8483F64d9C6d1EcF9b849Ae677dD3315835cb2
+// Member: 0x4B20993Bc481177ec7E8f571ceCaE8A9e22C02db
+// personal trainer: 0x78731D3Ca6b7E34aC0F824c42a7cC18A495cabaB
